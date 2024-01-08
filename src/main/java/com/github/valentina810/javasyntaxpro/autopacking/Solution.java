5e2f097146afe3e1fcfbe6b2e10147eb47d60ecf
valentinakolesnikova <polikarpova-v@yandex.ru>
2024-01-08 18:49:22 +0300

    feat: add javasyntaxpro 4.3 level

---
+++ src/main/java/com/github/valentina810/javasyntaxpro/autopacking/Solution.java
@@ -0,0 +1,41 @@
+package src.main.java.com.github.valentina810.javasyntaxpro.autopacking;
+
+/**
+ * task12.task1201
+ * В классе Solution представлены 8 переменных примитивных типов с именами вида типValue.
+ * Для каждой из этих переменных необходимо создать объекты-обертки и присвоить им имена вида типValueBox.
+ * Для создания объектов-оберток используй метод Тип.valueOf(примитив).
+ * <p>
+ * Пример переменной объекта-обертки: Integer integerValueBox = ....;
+ * <p>
+ * Требования:
+ * В классе Solution должны быть 8 переменных примитивных типов данных с именами вида типValue.
+ * В классе Solution должны быть 8 объектов типа класс-обертка примитивного типа с именами вида типValueBox.
+ * При создании объектов-оберток должны использоваться существующие переменные примитивных типов и метод Тип.valueOf(примитив).
+ * public class Solution {
+ * byte byteValue;
+ */
+public class Solution {
+    byte byteValue;
+    short shortValue;
+    int intValue;
+    long longValue;
+
+    float floatValue;
+    double doubleValue;
+
+    char charValue;
+    boolean booleanValue;
+
+    //напишите тут ваш код
+    Byte byteValueBox = Byte.valueOf(byteValue);
+    Short shortValueBox = Short.valueOf(shortValue);
+    Integer integerValueBox = Integer.valueOf(intValue);
+    Long longValueBox = Long.valueOf(longValue);
+
+    Float floatValueBox = Float.valueOf(floatValue);
+    Double doubleValueBox = Double.valueOf(doubleValue);
+
+    Character characterValueBox = Character.valueOf(charValue);
+    Boolean booleanValueBox = Boolean.valueOf(booleanValue);
+}
